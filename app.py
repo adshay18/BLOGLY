3e68cff516755150726e0e7a2a10a3e23d0c7d19
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-01-25 11:58:45 -0500

    fully functional, but ugly, flask application

---
+++ app.py
@@ -0,0 +1,99 @@
+"""Blogly application."""
+
+from flask import Flask, request, render_template, redirect, flash
+from models import db, connect_db, User
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+'''Configure app, currently development mode'''
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "blogly"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def redirect_to_users():
+    '''Redirect to /users'''
+    return redirect('/users')
+
+@app.route('/users')
+def list_users():
+    '''Show list of all users in the db'''
+    users = User.query.all()
+    return render_template('list.html', users=users)
+
+@app.route('/users/new')
+def show_form():
+    '''Show form to add a new user'''
+    return render_template('new_user.html')
+    
+@app.route('/users/new', methods=["POST"])
+def generate_new_user():
+    '''Capture form data and add new user to db'''
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    image_url = request.form["image_url"]
+    
+    if not image_url:
+        image_url = 'https://bitsofco.de/content/images/2018/12/broken-1.png'
+    if not first_name:
+        flash('Users must have both a First Name and a Last Name, please try again.')
+        return render_template('new_user.html')
+    if not last_name:
+        flash('Users must have both a First Name and a Last Name, please try again.')
+        return render_template('new_user.html')
+    
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return redirect(f'/users/{new_user.id}')
+
+@app.route('/users/<int:user_id>')
+def show_user(user_id):
+    '''Show name and profile pic of a user'''
+    user = User.query.get_or_404(user_id)
+    return render_template("user_details.html", user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    '''Edit details for a user'''
+    user = User.query.get_or_404(user_id)
+    return render_template('edit_user.html', user=user)
+    
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def change_user(user_id):
+    '''Update user information in db and display it correctly'''
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    image_url = request.form.get("image_url")
+    user = User.query.get_or_404(user_id)
+    
+    if not first_name:
+        first_name = user.first_name
+    if not last_name:
+        last_name = user.last_name
+    if not image_url:
+        image_url = user.image_url
+    
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+    
+    db.session.add(user)
+    db.session.commit()
+   
+    return redirect(f'/users/{user_id}')
+    
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def delete_user(user_id):
+    '''Delete a user from db'''
+    User.query.filter_by(id=user_id).delete()
+    db.session.commit()
+    return redirect('/')
---
+++ test_flask.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+from app import app
+from models import db, User
+
+# please create a testing db 'user_test' to test this application
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///user_test'
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+db.drop_all()
+db.create_all()
+
+class UserFlaskTestCase(TestCase):
+    '''Test routes for Users.'''
+    
+    def setUp(self):
+        '''Clean up existing users'''
+        User.query.delete()
+        
+    def tearDown(self):
+        '''Clean up failed transactions'''
+        db.session.rollback()
+        
+    def test_redirect_to_users(self):
+        '''Make sure homepage redirects to user list'''
+        with app.test_client() as client:
+            resp = client.get('/')
+            
+            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(resp.location, "http://localhost/users")
+            
+    def test_show_form(self):
+        '''Displays correct form'''
+        with app.test_client() as client:
+            resp = client.get('/users/new')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Last Name:', html)
+            
+    def test_generate_new_user(self):
+        '''New user should display new page with its associated info'''
+        with app.test_client() as client:
+            resp = client.post('/users/new',
+                               data={'first_name': 'Test',
+                                     'last_name': 'User', 'image_url': ''}, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<p><b>Test User</b></p>', html)
+            self.assertIn('<img src="https://bitsofco.de/content/images/2018/12/broken-1.png"', html)
+            
+    def test_edit_user(self):
+        '''shows edit user form'''
+        user = User(first_name="Test", last_name="Edit", image_url="https://www.outbrain.com/techblog/wp-content/uploads/2017/05/road-sign-361513_960_720.jpg")
+        db.session.add(user)
+        db.session.commit()
+        
+        with app.test_client() as client:
+            
+            resp = client.get(f'/users/{user.id}/edit?')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn(f'placeholder="{user.first_name}"', html)
