149d9ed1d057bf01f7cc1b5a44ebfa25e61fda03
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-01-31 15:10:55 -0500

    add models for tags and post tags

--- models.py
+++ models.py
@@ -34,3 +34,19 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.datetime.utcnow)
     
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    tags = db.relationship('Tag', secondary="posttags", backref="posts")
+    
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String, unique=True)
+    
+class PostTag(db.Model):
+    __tablename__ = 'posttags'
+    
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
